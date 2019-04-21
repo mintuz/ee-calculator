d90f4feecb8e961d05dc6311215bca3fb20fdb65
Adam Bulmer <mintuz1990@gmail.com>
2019-04-21 11:25:27 +0100

    Subtract implementation

--- src/libs/Calculate.js
+++ src/libs/Calculate.js
@@ -2,4 +2,7 @@ export default class Calculate {
     static add(previousValue, toAdd) {
         return previousValue + toAdd;
     }
+    static subtract(previousValue, toSubtract) {
+        return previousValue - toSubtract;
+    }
 }
