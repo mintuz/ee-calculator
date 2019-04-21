9c1cd5727241f691d1f8d9ebab1990eb7c85c5fc
Adam Bulmer <mintuz1990@gmail.com>
2019-04-21 11:31:59 +0100

    Buttons test

--- src/__tests__/Calculator.test.jsx
+++ src/__tests__/Calculator.test.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import Calculator from '../Calculator';
-import { render, cleanup } from 'react-testing-library';
+import { render, cleanup, fireEvent } from 'react-testing-library';
 
 describe('Calculator', () => {
 
@@ -10,4 +10,17 @@ describe('Calculator', () => {
         const { getByTestId } = render(<Calculator />)
         expect(getByTestId('ee-result__box').value).toEqual("0");
     });
+
+    test('digit buttons are available and clickable', () => {
+        const {getByText, getByTestId} = render(<Calculator />)
+
+        const availableDigits = ['9', '8', '7', '6', '5', '4', '3', '2', '1', '0'];
+
+        availableDigits.forEach(digit => {
+          const digitButton = getByText(digit)
+          fireEvent.click(digitButton);
+        })
+
+        expect(getByTestId('ee-result__box').value).toEqual("9876543210");
+      });
 });
