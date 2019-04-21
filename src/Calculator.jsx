be128eac2eca7871c0542a1ae7d28001dc6c1cbe
Adam Bulmer <mintuz1990@gmail.com>
2019-04-21 11:37:07 +0100

    Calculator output result to user on click of button

--- src/Calculator.jsx
+++ src/Calculator.jsx
@@ -1,10 +1,45 @@
 import React, { useState } from 'react';
 import Result from './components/Result';
+import Button from './components/Button';
 
 const Calculator = () => {
-    const [currentResult] = useState(0);
+    const [currentResult, setResult] = useState(0);
+    const [previousActions, setAction ] = useState([]);
+
+    const calculate = operation => {
+        const newActions = [...previousActions, operation];
+
+        setAction(newActions);
+        setResult(newActions.join(''));
+    };
+
     return (
-        <Result value={currentResult} />
+        <React.Fragment>
+            <Result value={currentResult} />
+            <div className="ee-c-calculator__controls">
+                <Button onClick={calculate}>AC</Button>
+                <Button onClick={calculate}>/</Button>
+
+                <Button onClick={calculate}>{7}</Button>
+                <Button onClick={calculate}>{8}</Button>
+                <Button onClick={calculate}>{9}</Button>
+                <Button onClick={calculate}>*</Button>
+
+                <Button onClick={calculate}>{4}</Button>
+                <Button onClick={calculate}>{5}</Button>
+                <Button onClick={calculate}>{6}</Button>
+                <Button onClick={calculate}>-</Button>
+
+                <Button onClick={calculate}>{1}</Button>
+                <Button onClick={calculate}>{2}</Button>
+                <Button onClick={calculate}>{3}</Button>
+                <Button onClick={calculate}>+</Button>
+
+                <Button onClick={calculate}>{0}</Button>
+                <Button onClick={calculate}>.</Button>
+                <Button onClick={calculate}>=</Button>
+            </div>
+        </React.Fragment>
     );
 };
 
