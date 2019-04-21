2a9de2027fc494ec2ba3f6f01631d8a9a4fb8813
Adam Bulmer <mintuz1990@gmail.com>
2019-04-21 11:50:53 +0100

    Add Visually hidden to result accessibility

--- src/components/Result.jsx
+++ src/components/Result.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const Result = props => {
     return (
         <div data-testid="ee-c-result">
-            <label htmlFor="result" data-testid="ee-result__label">Result:</label>
+            <label className="ee-u-vh" htmlFor="result" data-testid="ee-result__label">Result:</label>
             <input id="result" disabled type="text" data-testid="ee-result__box" value={props.value} />
         </div>
     );
