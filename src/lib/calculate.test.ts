660f3aa7db33c618356f50faaf0afcd7a01e13f1
Adam Bulmer <mintuz1990@gmail.com>
2022-09-06 19:27:49 +0100

    subtract lib test

--- src/lib/calculate.test.ts
+++ src/lib/calculate.test.ts
@@ -1,7 +1,11 @@
-import { add } from "./calculate";
+import { add, subtract } from "./calculate";
 
 describe("Calculate lib", () => {
   test("Should add value to previous value", () => {
     expect(add(10, 10)).toEqual(20);
+  });
+
+  test("Should subtract value from previous value", () => {
+    expect(subtract(10, 10)).toEqual(0);
   });
 });
--- src/lib/calculate.ts
+++ src/lib/calculate.ts
@@ -1,3 +1,7 @@
 export const add = (previousValue: number, toAdd: number) => {
   return previousValue + toAdd;
 };
+
+export const subtract = (previousValue: number, toSubtract: number) => {
+  return previousValue - toSubtract;
+};
