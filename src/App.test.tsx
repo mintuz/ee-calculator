d5d9f67b43be8fcb66395277d505e851dfc010f4
Adam Bulmer <mintuz1990@gmail.com>
2022-09-14 10:42:04 +0100

    calculator buttons clickable

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -6,16 +6,28 @@ test("Output is initially set to 0", () => {
   expect(screen.getByText("The calculated result is 0")).toBeInTheDocument();
 });
 
-test("Digit buttons are available and clickable", () => {
+test("Calculator buttons are available and clickable", () => {
   render(<App />);
 
-  const availableDigits = ["9", "8", "7", "6", "5", "4", "3", "2", "1", "0"];
+  const availableDigits = [
+    "9",
+    ".",
+    "8",
+    "7",
+    "6",
+    "5",
+    "4",
+    "3",
+    "2",
+    "1",
+    "0",
+  ];
 
   availableDigits.forEach((digit) => {
     fireEvent.click(screen.getByText(digit));
   });
 
   expect(
-    screen.getByText("The calculated result is 9876543210")
+    screen.getByText("The calculated result is 9.876543210")
   ).toBeInTheDocument();
 });
--- src/Calculator.tsx
+++ src/Calculator.tsx
@@ -1,3 +1,4 @@
+import { FC, useState } from "react";
 import styled from "styled-components";
 
 const StyledCalculator = styled.div`
@@ -10,29 +11,73 @@ const StyledResult = styled.div`
   padding: 16px;
 `;
 
+type CalculatorOperations =
+  | 1
+  | 2
+  | 3
+  | 4
+  | 5
+  | 6
+  | 7
+  | 8
+  | 9
+  | 0
+  | "+"
+  | "-"
+  | "*"
+  | "/"
+  | "="
+  | "AC";
+
+type ButtonProps = {
+  children: string | number;
+  onClick: (operation: CalculatorOperations) => void;
+};
+
+const Button: FC<ButtonProps> = ({ children, onClick }) => {
+  return (
+    <button
+      onClick={() => {
+        onClick(children);
+      }}
+    >
+      {children}
+    </button>
+  );
+};
+
 export const Calculator = () => {
+  const [previousActions, setAction] = useState<CalculatorOperations[]>([]);
+  const [currentResult, setResult] = useState<string>("0");
+
+  const calculate = (operation: CalculatorOperations) => {
+    const newActions = [...previousActions, operation];
+    setAction(newActions);
+    setResult(newActions.join(""));
+  };
+
   return (
     <StyledCalculator>
-      <StyledResult>The calculated result is 0</StyledResult>
+      <StyledResult>The calculated result is {currentResult}</StyledResult>
       <div>
-        <button>1</button>
-        <button>2</button>
-        <button>3</button>
-        <button>4</button>
-        <button>5</button>
-        <button>6</button>
-        <button>7</button>
-        <button>8</button>
-        <button>9</button>
-        <button>.</button>
+        <Button onClick={calculate}>0</Button>
+        <Button onClick={calculate}>1</Button>
+        <Button onClick={calculate}>2</Button>
+        <Button onClick={calculate}>3</Button>
+        <Button onClick={calculate}>4</Button>
+        <Button onClick={calculate}>5</Button>
+        <Button onClick={calculate}>6</Button>
+        <Button onClick={calculate}>7</Button>
+        <Button onClick={calculate}>8</Button>
+        <Button onClick={calculate}>9</Button>
+        <Button onClick={calculate}>.</Button>
+        {/* <Button>+</Button>
+        <Button>-</Button>
+        <Button>/</Button>
+        <Button>*</Button>
 
-        <button>+</button>
-        <button>-</button>
-        <button>/</button>
-        <button>*</button>
-
-        <button>=</button>
-        <button>AC</button>
+        <Button>=</Button>
+        <Button>AC</Button> */}
       </div>
     </StyledCalculator>
   );
